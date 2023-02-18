a593a95ec04a55086ee5a0a35091433dd5edf44f
kiran <Kiran.dubbewar18@users.noreply.github.com>
2023-02-18 20:35:32 +0530

    trail 1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,6 +5,14 @@ public class Email {
     private String emailId;
     private String password;
 
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
@@ -19,6 +27,32 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
+        int len=0;
+        int upper=0;
+        int lower=0,dig=0,spchar=0;
+        int size=newPassword.length();
+
+        if(this.password==oldPassword)
+        {
+            if(size<8) return;
+            for(int i=-0; i<newPassword.length(); i++)
+            {
+                len=1;
+                char ch = newPassword.charAt(i);
+                if(ch>64 && ch<91 ) upper=1;
+                if(ch>96 && ch <123) lower=1;
+                if((ch>=32 && ch <=47) || (ch>= 58 && ch<=64) || (ch>=91 && ch<=96) ||(ch <=123 && ch>=126))
+                     spchar=1;
+                if(ch>=48 && ch <=57)  dig = 1;
+
+                if(len==1 && upper==1 && lower==1 && dig==1 && spchar==1 && size>=8)
+                {
+                    this.password=newPassword;
+                }
+
+            }
+
+        }
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -2,25 +2,42 @@ package com.driver;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.*;
 
 public class Gmail extends Email {
 
     int inboxCapacity; //maximum number of mails inbox can store
+    PriorityQueue <mail> inboxQ = new LinkedList<>();
+    PriorityQuueue<mail> trashQ = new LinkedList<>();
+
+
+
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
     public Gmail(String emailId, int inboxCapacity) {
-
+        super(emailId);
+        this.inboxCapacity=inboxCapacity;
     }
 
     public void receiveMail(Date date, String sender, String message){
         // If the inbox is full, move the oldest mail in the inbox to trash and add the new mail to inbox.
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
-        // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-
+        // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already
+        mail m = new mail(date,sender,message);
+        if(inboxQ.size()==inboxCapacity)
+        {
+            mail t= inboxQ.peek();
+            trashQ.offer(t);
+            inboxQ.poll();
+            inboxQ.offer(m);
+        }
+        else
+           inboxQ.offer(m);
     }
 
     public void deleteMail(String message){
+
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
 
@@ -29,12 +46,18 @@ public class Gmail extends Email {
     public String findLatestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
-
+        if(inboxQ.isEmpty()) return null;
+        else
+            return inboxQ.peek(message);
     }
 
     public String findOldestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the oldest mail present in the inbox
+        if(inboxQ.size()==0) return null;
+        else {
+           return inboxQ.peek(message);
+        }
 
     }
 
@@ -46,20 +69,35 @@ public class Gmail extends Email {
 
     public int getInboxSize(){
         // Return number of mails in inbox
-
+         return inboxQ.size();
     }
 
     public int getTrashSize(){
         // Return number of mails in Trash
-
+        return trashQ.size();
     }
 
     public void emptyTrash(){
         // clear all mails in the trash
+        trashQ.clear();
 
     }
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
+        return this.inboxCapacity;
     }
 }
+class mail{
+    Date date;
+    String senderId;
+    String massage;
+    mail(Date date, String senderId, String massage)
+    {
+        this.date=date;
+        this.senderId=senderId;
+        this.massage=massage;
+    }
+
+}
+
